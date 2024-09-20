e6a2f4feaab957d621ea670d0bb0fa18fe8f680d
ShiiFt38 <maseemet0@gmail.com>
2024-09-20 14:01:08 +0200

    Login page added

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtWidgets import *
-
+from views.login_dialog import Login
+from PyQt5.QtCore import Qt
 
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(900, 700)
+        self.resize(600, 600)
         self.setWindowTitle("Aquatic Weeds Predictive System")
+        self.setWindowIcon(QIcon("Assets/Images/Logo.png"))
+        self.display_login()
+
+    def display_login(self):
+        login_dialog = Login()
+
+        login_dialog.exec()
 
 
 if __name__ == "__main__":
     app = QApplication([])
     main_win = App()
-    main_win.show()
     app.exec()
